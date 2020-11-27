b9144fc5faba37954ebf9185057a76e194bdc45b
czh9919 <czh9919@outlook.com>
2020-11-27 22:04:21 +0800

    add button and change about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p> Hello, this is an about page. </p>
+    <p> Here is something you need to know</p>
+    <p> Code won't be saved on this page, please save your code on your device </p>
+    <p> When you reload or click the reload button, everything you have done on the page will be lost.</p> 
   </div>
 </template>
--- src/views/database.vue
+++ src/views/database.vue
@@ -5,19 +5,22 @@
         <div><h3>Database Page</h3></div>
       </el-row>
     </div>
-    <p>
+    <div>
       <el-input
-  type="textarea"
-  :autosize="{ minRows: 10, maxRows: 200}"
-  placeholder="请输入代码"
-  v-model="textarea1">
-</el-input>
-    <el-button
-    plain
-    @click="open1">
-    运行
-  </el-button>
-    </p>
+        type="textarea"
+        :autosize="{ minRows: 10, maxRows: 200 }"
+        resize="both"
+        placeholder="请输入代码"
+        v-model="textarea1"
+      >
+      </el-input>
+      <div style="margin: 20px 0;"></div>
+      <el-button plain @click="open1" type="primary"> run </el-button>
+      <el-button plain @click="reload" type="danger"> reload </el-button>
+    </div>
+    <el-backtop
+      target=".page-component__scroll .el-scrollbar__wrap"
+    ></el-backtop>
   </div>
 </template>
 
@@ -25,37 +28,50 @@
 // @ is an alias to /src
 
 export default {
-  name: 'database',
+  name: "database",
   data() {
     return {
-      textarea1: ''
-    }
+      textarea1: "",
+    };
   },
-  methods:{
-      open1:function() {
-        let that=this;
-        const h = this.$createElement;
-        let res=this.$alasql.promise(this.textarea1)
-        .then(function(res){
-        console.log(res);
-        console.log(JSON.stringify(res));
+  methods: {
+    reload:function() {
+      location.reload();
+    },
+    open1: function () {
+      let that = this;
+      const h = this.$createElement;
+      if (this.textarea1 == "") {
         that.$notify({
-          title: 'Result',
-          message: h('i', { style: 'color: teal'}, JSON.stringify(res)),
-          duration: 0
+          title: "Result",
+          message: h(
+            "i",
+            { style: "color: teal" },
+            "NO INPUT, PLEASE ENTER CODE"
+          ),
         });
-    }).catch(function(err)
-      {
-        that.$notify({
-          title: 'Result',
-          message: h('i', { style: 'color: teal'}, err.toString())
+        return;
+      }
+      let res = this.$alasql
+        .promise(this.textarea1)
+        .then(function (res) {
+          console.log(res);
+          console.log(JSON.stringify(res));
+          that.$notify({
+            title: "Result",
+            message: h("i", { style: "color: teal" }, JSON.stringify(res)),
+            duration: 0,
+          });
+        })
+        .catch(function (err) {
+          that.$notify({
+            title: "Result",
+            message: h("i", { style: "color: teal" }, err.toString()),
+          });
+          console.log(err.toString());
         });
-        console.log(err.toString())
-        }
-    );
-        
-      },
-  }
-}
+    },
+  },
+};
 </script>
 
